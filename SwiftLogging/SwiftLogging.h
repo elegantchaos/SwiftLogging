331a3f27df6de64ef0865222d30151e50cb8dd63
Sam Deane <sam@elegantchaos.com>
2014-09-04 21:53:20 +0100

    initial project

---
+++ SwiftLogging/SwiftLogging.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLogging.h
+//  SwiftLogging
+//
+//  Created by Sam Deane on 04/09/2014.
+//  Copyright (c) 2014 Elegant Chaos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftLogging.
+FOUNDATION_EXPORT double SwiftLoggingVersionNumber;
+
+//! Project version string for SwiftLogging.
+FOUNDATION_EXPORT const unsigned char SwiftLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLogging/PublicHeader.h>
+
+
